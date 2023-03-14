d3f9d0154cf54c532871c2e4aaab185390226835
ShenMian <sms_school@outlook.com>
2023-03-14 08:19:42 +0800

    .

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -16,16 +16,14 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
   -- Set colorscheme to use
-  colorscheme = "vscode", -- default: "astrodark"
-
+  -- colorscheme = "vscode", -- default: "astrodark"
+  colorscheme = "astrodark",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
     underline = true,
   },
-
   lsp = {
     -- customize lsp formatting options
     formatting = {
@@ -52,7 +50,6 @@ return {
       -- "pyright"
     },
   },
-
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -63,7 +60,6 @@ return {
       },
     },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
