e3dc281a966174916c5a6918b092e659a6f7bd8f
ShenMian <sms_school@outlook.com>
2023-03-15 17:23:16 +0800

    .

--- update.sh
+++ update.sh
@@ -5,20 +5,23 @@ cd "$( cd "$( dirname "$0"  )" && pwd  )" || exit
 # zsh
 ln -sf "$PWD/.zshrc" ~/.zshrc
 
-# kitty
-ln -sf "$PWD/kitty" ~/.config/
-
 # lf
 ln -sf "$PWD/lf" ~/.config/
 
 # neovim
 ln -sf "$PWD/astronvim" ~/.config/nvim/lua/user
 
-# tmux
-ln -sf "$PWD/tmux/.tmux.conf.local" ~/.tmux.conf.local
-
 # lazygit
 ln -sf "$PWD/lazygit/config.yml" ~/.config/lazygit/config.yml
 
+# wezterm
+ln -sf "$PWD/wezterm" ~/.config/
+
+# kitty
+ln -sf "$PWD/kitty" ~/.config/
+
 # alacritty
 # ln -sf "$PWD/alacritty" ~/.config/
+
+# tmux
+ln -sf "$PWD/tmux/.tmux.conf.local" ~/.tmux.conf.local
