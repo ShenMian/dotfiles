54b3f063523bffd4e11786ea44e7f0db88a8b2a5
ShenMian <sms_school@outlook.com>
2023-03-15 21:10:37 +0800

    .

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,13 @@
 local wezterm = require("wezterm")
 return {
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	font_size = 12.0,
+	color_scheme = "Tango (terminal.sexy)",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	-- window_background_opacity = 0.93,
 }
