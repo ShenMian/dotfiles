c6a63537cbb6d249a670abfcac0c2b2a8b4aa5f2
ShenMian <sms_school@outlook.com>
2023-03-20 10:37:21 +0800

    .

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,8 @@ return {
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
 	font_size = 12.0,
 	color_scheme = "Tango (terminal.sexy)",
+	initial_cols = 120,
+	initial_rows = 30,
 	window_padding = {
 		left = 0,
 		right = 0,
