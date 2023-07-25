3ecb7c451603576b67b251738c8fe52c6fe0c378
ShenMian <sms_school@outlook.com>
2023-07-25 11:33:25 +0800

    .

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,3 @@
-### Prompt
-
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
-
-### Chocolatey
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-### Key bindings
-
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
@@ -23,19 +5,43 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineOption -HistorySearchCursorMovesToEnd
 Set-PSReadlineOption -ShowToolTips
 
+### Chocolatey
+
+# tab-completions to function for `choco`.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
 ### Environment
 
+# proxy
 $Env:http_proxy = "127.0.0.1:7890"
 $Env:https_proxy = "127.0.0.1:7890"
 
 ### Alias
 
+Set-Alias -Name c -Value clear
+
+# bat
 Set-Alias -Name cat -Value bat -Option AllScope
+
+# exa
 Set-Alias -Name ls -Value exa -Option AllScope
 Set-Alias -Name l -Value exa
 function ll { exa -la }
-Set-Alias -Name c -Value clear
+
+# neovim
 Set-Alias -Name v -Value nvim
+
+# neovide
 function vi { neovide --multigrid $args }
+
+# lazygit
 Set-Alias -Name lg -Value lazygit
 
+### Prompt
+
+# Starship
+Invoke-Expression (&starship init powershell)
