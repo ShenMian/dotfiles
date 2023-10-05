8f361f92296f2a0a12533a49b344299dd54854de
ShenMian <sms_school@outlook.com>
2023-10-05 14:48:59 +0800

    .

--- astronvim/options.lua
+++ astronvim/options.lua
@@ -3,10 +3,10 @@ local options = {
   opt = {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    number = true,         -- sets vim.opt.number
+    spell = false,         -- sets vim.opt.spell
+    signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+    wrap = false,          -- sets vim.opt.wrap
     scrolloff = 5,
     tabstop = 4,
     shiftwidth = 4,
@@ -14,21 +14,21 @@ local options = {
     expandtab = true,
   },
   g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ",                 -- sets vim.g.mapleader
+    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true,              -- enable completion at start
+    autopairs_enabled = true,        -- enable autopairs at start
+    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    instant_username = "sms", -- instant.nvim
+    instant_username = "sms",        -- instant.nvim
   },
 }
 
 -- Neovide configuration
 if vim.fn.exists "g:neovide" then
-  -- local_vim.opt.guifont = { "Cascadia Code PL", ":h12" }
-  options.opt.guifont = { "CaskaydiaCove Nerd Font", ":h12" }
+  -- options.opt.guifont = { "Cascadia Code PL", ":h12" }
+  options.opt.guifont = { "CaskaydiaCove Nerd Font", ":h14" }
   options.g.neovide_hide_mouse_when_typing = true
   options.g.neovide_fullscreen = false
 
