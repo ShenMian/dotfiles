cb44158e83b1f34f7ca6be4af08ce8335cf32b30
ShenMian <70068015+ShenMian@users.noreply.github.com>
2024-05-10 15:03:25 +0800

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,24 @@
-local wezterm = require("wezterm")
-return {
-	font = wezterm.font("CaskaydiaCove Nerd Font"),
-	font_size = 12.0,
-	color_scheme = "Tango (terminal.sexy)",
-	initial_cols = 120,
-	initial_rows = 30,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	-- window_background_opacity = 0.93,
-	hide_tab_bar_if_only_one_tab = true,
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'CaskaydiaCove Nerd Font'
+config.font_size = 12.0
+
+config.color_scheme = 'Tango (terminal.sexy)'
+config.window_background_opacity = 0.85
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
+config.initial_cols = 120
+config.initial_rows = 30
+
+config.enable_scroll_bar = true
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.default_prog = { 'powershell.exe', '-NoLogo' }
+end
+
+return config
