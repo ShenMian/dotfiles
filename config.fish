c5e5f4cd03c489fc15f787db1003a06f601602dd
ShenMian <sms_school@outlook.com>
2024-06-18 00:49:05 +0800

    .

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 # Environment
-set -Ux all_proxy 'socks5://127.0.0.1:7890/'
+set -Ux all_proxy 'socks://127.0.0.1:7890/'
 set -Ux http_proxy 'http://127.0.0.1:7890/'
 set -Ux https_proxy $http_proxy
 set -Ux no_proxy 'localhost, 127.0.0.0/8, ::1'
@@ -9,7 +9,7 @@ set -x TERM xterm-256color
 set -Ux VISUAL 'nvim'
 set -Ux EDITOR 'nvim'
 
-fish_add_path -Ua '/home/sms/.cargo/bin/'
+fish_add_path -Ua "$HOME/.cargo/bin/"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
