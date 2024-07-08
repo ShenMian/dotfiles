e2f44a2b1fb2f0bd633bba9303bbd23b5a5cc863
ShenMian <sms_school@outlook.com>
2024-07-08 18:50:15 +0100

    feat(neovim): add copilot-lua

--- astronvim/lua/community.lua
+++ astronvim/lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.pack.cpp" },
