d2bd02921961b19401320f71554973dd7718bb28
ShenMian <sms_school@outlook.com>
2025-01-31 13:05:10 +0000

    .

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0"  )" && pwd  )" || exit
+
+# fish
+ln -sf "$PWD/config.fish" ~/.config/fish/config.fish
+
+# neovim
+ln -sf "$PWD/astronvim" ~/.config/nvim
+
+# lazygit
+ln -sf "$PWD/lazygit/config.yml" ~/.config/lazygit/config.yml
+
+# wezterm
+ln -sf "$PWD/wezterm" ~/.config/
+
+# helix
+ln -sf "$PWD/helix" ~/.config/
+
+# alacritty
+ln -sf "$PWD/alacritty" ~/.config/
+
+# ghostty
+# ln -sf "$PWD/ghostty" ~/.config/
+
+# kitty
+# ln -sf "$PWD/kitty" ~/.config/
+
+# lf
+# ln -sf "$PWD/lf" ~/.config/
+
+# tmux
+# ln -sf "$PWD/tmux/.tmux.conf.local" ~/.tmux.conf.local
+
+# zsh
+# ln -sf "$PWD/.zshrc" ~/.zshrc
+
