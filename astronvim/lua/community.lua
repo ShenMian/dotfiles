10505f5ce9d7fcb5b4d0ef323bdfc8b6f09b1499
ShenMian <sms_school@outlook.com>
2025-03-06 01:01:30 +0000

    .

--- astronvim/lua/community.lua
+++ astronvim/lua/community.lua
@@ -11,18 +11,19 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.editing-support.vim-visual-multi" },
 
-  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.workflow.hardtime-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.recipes.neovide" },
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
 
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.markdown-and-latex.markview-nvim" },
   { import = "astrocommunity.pack.typst" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.pack.haskell" },
   -- import/override with your plugins folder
 }
