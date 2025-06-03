88a2acf5c78a1f13bd00b38d611d8365741f8f9e
ShenMian <sms_school@outlook.com>
2025-06-03 10:01:52 +0800

    .

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
 ### PowerShell
-
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
@@ -8,14 +7,12 @@ Set-PSReadlineOption -HistorySearchCursorMovesToEnd
 Set-PSReadlineOption -ShowToolTips
 
 ### Environment
-
 $Env:all_proxy = "127.0.0.1:7897"
 $Env:http_proxy = "127.0.0.1:7897"
 $Env:https_proxy = $Env:http_proxy
 $Env:no_proxy = "localhost,127.0.0.0/8,::1"
 
 ### Alias
-
 Set-Alias -Name c -Value clear
 
 # eza
@@ -31,13 +28,11 @@ if (Get-Command bat -ErrorAction SilentlyContinue) {
     function cat { bat -pp @args }
 }
 
-# editors
 Set-Alias -Name v -Value nvim
 Set-Alias -Name vi -Value neovide
 Set-Alias -Name hx -Value helix -Option AllScope
-
-# lazygit
 Set-Alias -Name lg -Value lazygit
+Set-Alias -Name youtube -Value yt-dlp -Option AllScope
 
 # scoop-search
 if (Get-Command scoop-search -ErrorAction SilentlyContinue) {
