8dc75afc81e11ff562d6c28242da1a5b0ea539c3
ShenMian <sms_school@outlook.com>
2026-03-09 14:49:54 +0000

    .

--- update.sh
+++ update.sh
@@ -9,29 +9,32 @@ ln -sf "$PWD/.fonts.conf" ~/.fonts.conf
 ln -sf "$PWD/config.fish" ~/.config/fish/config.fish
 ln -sf "$PWD/secrets.fish" ~/.config/fish/secrets.fish
 
+# mpv
+ln -sf "$PWD/mpv" ~/.config/
+
+# alacritty
+ln -sf "$PWD/alacritty" ~/.config/
+
+# ghostty
+# ln -sf "$PWD/ghostty" ~/.config/
+
+# kitty
+# ln -sf "$PWD/kitty" ~/.config/
+
+# wezterm
+# ln -sf "$PWD/wezterm" ~/.config/
+
 # neovim
 ln -sf "$PWD/astronvim" ~/.config/nvim
 
 # neovide
 ln -sf "$PWD/neovide" ~/.config/
 
+# helix
+# ln -sf "$PWD/helix" ~/.config/
+
 # lazygit
 ln -sf "$PWD/lazygit/config.yml" ~/.config/lazygit/config.yml
-
-# wezterm
-ln -sf "$PWD/wezterm" ~/.config/
-
-# helix
-ln -sf "$PWD/helix" ~/.config/
-
-# alacritty
-ln -sf "$PWD/alacritty" ~/.config/
-
-# ghostty
-ln -sf "$PWD/ghostty" ~/.config/
-
-# kitty
-# ln -sf "$PWD/kitty" ~/.config/
 
 # lf
 # ln -sf "$PWD/lf" ~/.config/
