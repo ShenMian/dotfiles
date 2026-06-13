c49e9d38e9090f8540d9d4759977bb7259377e3c
ShenMian <sms_school@outlook.com>
2026-06-13 14:47:46 +0800

    .

--- config.fish
+++ config.fish
@@ -6,7 +6,7 @@ function set_proxy -d "Set proxy environment variables"
     set -gx all_proxy "socks://$PROXY_SERVER/"
     set -gx http_proxy "http://$PROXY_SERVER/"
     set -gx https_proxy $http_proxy
-    set -gx no_proxy "localhost,127.0.0.0/8,::1"
+    set -gx no_proxy "localhost,127.0.0.0/8,::1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12"
 
     # Uppercase for compatibility
     set -gx ALL_PROXY $all_proxy
@@ -33,9 +33,11 @@ set_proxy
 
 # set -x TERM xterm-256color
 
-set -gx VISUAL "nvim"
-set -gx EDITOR "nvim"
-set -gx MANPAGER "nvim +Man!"
+if type -q nvim
+    set -gx VISUAL "nvim"
+    set -gx EDITOR "nvim"
+    set -gx MANPAGER "nvim +Man!"
+end
 
 set -gx GPG_TTY (tty)
 
@@ -52,23 +54,44 @@ if status is-interactive
     ## Alias
 
     # Utility aliases
-    alias ls="eza"
-    alias l="ls"
-    alias ll="eza --long --icons"
-    alias tree="ls --tree"
-    alias cat="bat -pp"
+    if type -q eza
+        alias ls="eza"
+        alias l="ls"
+        alias ll="eza --long --icons"
+        alias tree="eza --tree"
+    else
+        alias l="ls"
+        alias ll="ls --long"
+    end
+    if type -q bat
+        alias cat="bat -pp"
+    end
     alias c="clear"
 
     # Editor aliases
-    alias zed="zeditor"
-    alias v="nvim"
-    alias vi="neovide"
-    alias hx="helix"
+    if type -q zeditor
+        alias zed="zeditor"
+    end
+    if type -q nvim
+        alias v="nvim"
+    end
+    if type -q neovide
+        alias vi="neovide"
+    end
+    if type -q helix
+        alias hx="helix"
+    end
 
     # Tool aliases
-    alias lg="lazygit"
-    alias youtube="yt-dlp"
-    alias open="xdg-open"
+    if type -q lazygit
+        alias lg="lazygit"
+    end
+    if type -q yt-dlp
+        alias youtube="yt-dlp"
+    end
+    if type -q xdg-open
+        alias open="xdg-open"
+    end
 end
 
 # Force standard key bindings (overrides implicit Vi mode from EDITOR=nvim)
